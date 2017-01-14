8c0c372496f99e893aebc25a2ade2243ea1c8344
XorNick <supernukebmp@gmail.com>
2017-01-14 07:46:29 -0500

    Initial commit

---
+++ src/main/java/dev/xornick/scarce/Scarce.java
@@ -0,0 +1,20 @@
+package dev.xornick.scarce;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Scarce extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(ChatColor.GREEN + "Hey what's up guys, it's Scarce here.");
+    }
+}
